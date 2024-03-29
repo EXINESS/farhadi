d313066389b34d071b384bfb4e8f7195dd1b06a4
fatemeh 22 <farhadifatemeh190@gmail.com>
2024-03-29 16:31:44 +0330

    add carousel

--- mine.js
+++ mine.js
@@ -1,1 +1,19 @@
- 
+var carousel = $(".carousel"),
+    index = 0;
+
+$(".next").no("click", { d: "n"}, rotate);
+
+function rotate(e){
+    if(e.data.d=="n"){
+        index = index - 60;
+    }
+    if(e.data.d=="n"){
+        index = index + 60;
+    }
+    carousel.css({
+        "-webkit-transform": "rotateY("+index+"deg)",
+        "-moz-transform": "rotateY("+index+"deg)",
+        "-o-transform": "rotateY("+index+"deg)",
+        "transform": "rotateY("+index+"deg)"
+    });
+}
